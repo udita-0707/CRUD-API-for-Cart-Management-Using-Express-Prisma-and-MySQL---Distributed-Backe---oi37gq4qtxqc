d1796313032f8cd7c12f80c1a5d9ef9f2f3ada8a
Udita <153092248+udita-0707@users.noreply.github.com>
2024-11-30 02:16:42 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,28 @@
-const { API_KEY } = require('../authkey');
+// const { API_KEY } = require('../authkey');
+
+// const authMiddleware = (req, res, next) => {
+//   const apiauthkey = req.headers['apiauthkey'];
+
+//   if (!apiauthkey || apiauthkey !== API_KEY) {
+//     return res.status(403).json({ error: 'apiauthkey is missing or invalid' });
+//   }
+
+//   next();
+// };
+
+// module.exports = authMiddleware;
+const validApiAuthKey = '8a60348b-d4a4-564a-9b45-aab518adb7f4';
 
 const authMiddleware = (req, res, next) => {
   const apiauthkey = req.headers['apiauthkey'];
-
-  if (!apiauthkey || apiauthkey !== API_KEY) {
-    return res.status(403).json({ error: 'apiauthkey is missing or invalid' });
+  if (!apiauthkey) {
+    return res.status(401).json({ message: 'Access denied, apiauthkey is missing' });
   }
-
+  if (apiauthkey !== validApiAuthKey) {
+    return res.status(403).json({ message: 'Forbidden, invalid apiauthkey' });
+  }
   next();
 };
 
 module.exports = authMiddleware;
+
